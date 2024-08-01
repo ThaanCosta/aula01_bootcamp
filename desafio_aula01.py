15b9441264156c28fc41d9d8aa1452b9323ca3bf
Projetos de Dados <thaancosta@gmail.com>
2024-08-01 13:52:34 -0300

    desfaio concluido

---
+++ desafio_aula01.py
@@ -0,0 +1,14 @@
+
+constante_bonus = 1000
+
+nome = input("Digite seu nome: ")
+
+salario = float(input("Digite do seu Salario: "))
+
+bonus = float(input("Digite do seu bonus: "))
+
+valor_do_bonus = constante_bonus + salario * bonus
+
+
+
+print(f"Nome: {nome} | Salario: {salario} | Bonus: {valor_do_bonus}")
