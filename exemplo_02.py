ae50ef2b66e15b42dd9855f5cd107e20a8f63f32
Projetos de Dados <thaancosta@gmail.com>
2024-08-01 13:35:25 -0300

    aulas do treinamento

---
+++ exemplo_02.py
@@ -0,0 +1,9 @@
+
+#print(int(input("Digite o primeiro numero: ")) + int(input("Digite o segundo numero: ")))
+
+numero = 3
+print(type(numero))
+# numero_decimal = 4.78
+# nome_de_usuario = "Luciano"
+# verdadeiro = True #False
+
